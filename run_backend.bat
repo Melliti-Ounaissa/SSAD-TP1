46da271c18ee2ffc9ba24c1f192e44782b703277
Melliti-Ounaissa <ounaissamelliti@gmail.com>
2025-10-15 15:01:53 +0100

    Added run_backend.bat

---
+++ run_backend.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Script to run the Flask backend server on Windows
+
+echo Starting Flask backend server...
+echo Server will run on http://localhost:5000
+echo.
+python backend/flask_app.py
---
+++ run_backend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script to run the Flask backend server
+
+echo "Starting Flask backend server..."
+echo "Server will run on http://localhost:5000"
+echo ""
+python backend/flask_app.py
